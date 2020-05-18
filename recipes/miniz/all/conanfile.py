fcb565295984b6348fbb1dd09b194a7f5b10e288
a4z <harald.achitz@gmail.com>
2020-05-18 15:54:43 +0200

    merge master

--- recipes/miniz/all/conanfile.py
+++ recipes/miniz/all/conanfile.py
@@ -46,11 +46,7 @@ class MinizConan(ConanFile):
         return self._cmake
 
     def build(self):
-<<<<<<< HEAD
-        for patch in self.conan_data["patches"][self.version]:
-=======
         for patch in self.conan_data.get("patches", {}).get(self.version, []):
->>>>>>> master
             tools.patch(**patch)
         cmake = self._configure_cmake()
         cmake.build()
